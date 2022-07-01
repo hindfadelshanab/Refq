6a3aeebf740557ee31fedfdb8ea698b40d3f4073
hindfadelshanab <hanoda.hanona@hotmail.com>
2022-07-01 16:58:25 +0300

    cooo

---
+++ app/src/main/java/com/devhind/qibla/refg/utilites/AppController.java
@@ -0,0 +1,41 @@
+package com.devhind.qibla.refg.utilites;
+
+import android.app.Application;
+import android.content.ComponentCallbacks2;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+
+public class AppController extends Application implements ComponentCallbacks2 {
+
+    private static AppController mInstance;
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+//        ViewPump.init(
+//                ViewPump.builder()
+//                        .addInterceptor(
+////                              new CalligraphyInterceptor(
+////                                        new CalligraphyConfig.Builder()
+////                                                .setDefaultFontPath("fonts/nunito_medium.ttf")
+////                                                .setFontAttrId(uk.co.chrisjenx.calligraphy.R.attr.fontPath)
+////                                                .build()
+////                                )
+//                        )
+//                        .build()
+   //     );
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
+
